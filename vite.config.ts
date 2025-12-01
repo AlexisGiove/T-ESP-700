b3a9e81b76edc38c3a573cad99068c0ba23f552e
AlexisGiove <alexis.giove@outlook.fr>
2025-12-01 10:29:10 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Si le repo GitHub s'appelle "argos-landing", décommenter la ligne suivante :
-  // base: '/argos-landing/',
-  // Sinon, laisser vide pour un déploiement à la racine
+  base: '/T-ESP-700/',
 })
 
 
